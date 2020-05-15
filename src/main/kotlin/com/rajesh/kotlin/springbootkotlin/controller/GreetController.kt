fb1e2983906c9e112e8847ec3d0dca6d1642a2ef
Rajesh <rajeshkannang87@gmail.com>
2020-05-15 07:46:08 +0530

    Changes to copy Docker File

--- src/main/kotlin/com/rajesh/kotlin/springbootkotlin/controller/GreetController.kt
+++ src/main/kotlin/com/rajesh/kotlin/springbootkotlin/controller/GreetController.kt
@@ -1,2 +1,15 @@
 package com.rajesh.kotlin.springbootkotlin.controller
 
+import com.rajesh.kotlin.springbootkotlin.model.Greet
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class GreetController {
+    val counter = AtomicLong()
+
+    @GetMapping("/greeting/{name}")
+    fun greet(@PathVariable name: String) = Greet(counter.incrementAndGet(), "Hello ${name}")
+}
--- src/main/kotlin/com/rajesh/kotlin/springbootkotlin/model/Greet.kt
+++ src/main/kotlin/com/rajesh/kotlin/springbootkotlin/model/Greet.kt
@@ -1,2 +1,3 @@
 package com.rajesh.kotlin.springbootkotlin.model
 
+data class Greet(val id:Long, val name: String)
